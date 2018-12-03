d206acfe3d8ec6b7d409df360add87cca34b6b83
Erjan <erjan@users.noreply.github.com>
2018-12-03 22:03:13 +0500

    adding another working pretty print function

--- print_2d_indices.py
+++ print_2d_indices.py
@@ -10,3 +10,24 @@ print_indices(r)
 [1][0] [1][1] [1][2] 
 [2][0] [2][1] [2][2]
 '''
+
+#better more fully covering function
+def print_indices(x):
+    print("the matrix, size: " + str(width) + "x" + str(height))    
+    if height == 1:
+        print("one liner")
+        for i in range(width):
+            for j in range(height):            
+                print("[" + str(i) + "]" + "[" + str(j) + "]", end='\t')    
+    elif width ==1:
+        print("column")
+        for i in range(width):
+            for j in range(height):            
+                print("[" + str(i) + "]" + "[" + str(j) + "]")    
+    else:
+        print("random nxm matrix")
+        print("width: " + str(width) + " ,height: " + str(height))
+        for i in range(height):
+            for j in range(width):            
+                print("[" + str(i) + "]" + "[" + str(j) + "]",end= '\t')
+            print()
