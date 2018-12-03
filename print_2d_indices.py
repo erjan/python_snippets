c0f7af2f15fe7b5206b7cb14ee9bcba61938e708
Erjan <erjan@users.noreply.github.com>
2018-12-03 19:24:37 +0500

    adding example of the output

--- print_2d_indices.py
+++ print_2d_indices.py
@@ -5,3 +5,8 @@ def print_indices(m):
             print("[" + str(i) + "]" + "[" + str(j) + "]", end = ' ')
         print()
 print_indices(r)
+'''
+[0][0] [0][1] [0][2] 
+[1][0] [1][1] [1][2] 
+[2][0] [2][1] [2][2]
+'''
